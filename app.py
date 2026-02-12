357c3b2e9af71e974d2564cfdc531fbdca350cee
eiumerefendipgeeplovdiv2024-blip <e.iumerefendi.pgee.plovdiv2024@gmail.com>
2026-02-12 10:31:58 +0200

    Update quiz questions and scoring logic

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ with st.form("quiz"):
     q1 = st.radio("1. Коя е най-голямата държава по площ?", ["Китай", "САЩ", "Русия", "Канада"])
     q2 = st.radio("2. Кой океан е най-голям?", ["Атлантически", "Индийски", "Северен ледовит", "Тихи"])
     q3 = st.radio("3. Кой е най-високият връх в света?", ["К2", "Еверест", "Килиманджаро", "Монблан"])
-    q4 = st.text_input("4. Коя е столицата на Франция?")
-    q5 = st.number_input("5. Колко континента има на Земята?", min_value=0, step=1)
+    q4 = st.text_input("4.Кой е най-големият континент по площ ?")
+    q5 = st.text_input("5.Коя е най-дългата река в света ?")
+    q6 = st.text_input("6. Коя държава има най-голямо население в света?")
+    q7 = st.text_input("7.Кой е най-големият пустинен район в света?")
+    q8 = st.text_input("8. Кой пролив разделя Европа от Азия при Истанбул?")
+    q9 = st.text_input("9. Коя е столицата на Франция?")
+    q10= st.number_input("10. Колко континента има на Земята?", min_value=0, step=1)
     
     submitted = st.form_submit_button("Провери")
 
@@ -16,13 +21,18 @@ if submitted:
     if q1 == "Русия": points += 1
     if q2 == "Тихи": points += 1
     if q3 == "Еверест": points += 1
-    if q4.strip().lower() in ["париж", "paris"]: points += 1
-    if q5 == 7: points += 1
+    if q4 == "Азия": points += 1
+    if q5 == "Нил": points += 1
+    if q6 == "Индия": points += 1
+    if q7 == "Сахара": points += 1
+    if q8 == "Босфорът": points += 1
+    if q9.== "Париж": points += 1
+    if q10 == 7: points += 1
 
-    if points == 5:
-        st.success(f"Отличен! Верни отговори: {points}/5")
-    elif points >= 3:
-        st.info(f"Добър резултат: {points}/5")
-    else:
-        st.error(f"Опитай пак: {points}/5")
+    if points == 10:
+        st.success(f"Отличен! Верни отговори: {points}/10")
+    elif points >= 5:
+        st.info(f"Добър резултат: {points}/10")
+    else 
+        st.error(f"Слаб: {points}/10")
       
